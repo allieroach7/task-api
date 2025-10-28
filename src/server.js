fd7f9e0ac91b62a41b7d9b4074e0ad9efe312bdb
Allie Roach <rroach5@charlotte.edu>
2025-10-27 23:07:44 -0400

    Add error handling for loading Swagger YAML specifications

--- src/server.js
+++ src/server.js
@@ -12,7 +12,7 @@ app.use(express.json());
 app.use(morgan('tiny'));
 
 try {
-  const specs = YAML.load('.\docs\openapi.yaml');
+  const specs = YAML.load('./docs/openapi.yaml');
   console.log('Swagger specs loaded successfully');
   app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(specs));
 } catch (error) {
